1ef58b3e255c319dfb5acbd0d3c7ce93f46a0303
fran496 <franciscorivas496@gmail.com>
2019-04-03 18:06:49 -0400

    Created a function to test the player input, reorganized the code lines into functions which have a single responsability

--- game.py
+++ game.py
@@ -1,11 +1,37 @@
+from random import *
+
 gameOn = False
-userName = str(raw_input("Hello! What's your name? "))
-print("Welcome " + userName + "!, write start when you're ready to play!")
+randomNumber = 0
 
-while gameOn == False:
-    startGame = str(raw_input("Ready? "))
-    if startGame == "start":
-        print("Let's play!")
-        gameOn = True
 
-# game()
+def testPlayerInput(playerInput):
+    if playerInput == randomNumber:
+        print("YOU WIN " + playerName + "!")
+        gameOn = False
+        return gameOn
+    elif playerInput < randomNumber:
+        print("THAT'S NOT IT! Guess higher!")
+        return
+    elif playerInput > randomNumber:
+        print("THAT'S NOT IT! Guess lower!")
+        return
+
+def game ():
+    print("I'm going to choose a natural number between 1 and 100, if you guess it right you win! ")
+    randomNumber = randint(1, 100)
+    
+    while gameOn == True:
+        playerInput = input()
+        testPlayerInput(playerInput)
+
+def intro ():
+    playerName = str(raw_input("Hello! What's your name? "))
+    print("Welcome " + playerName + "!, write start when you're ready to play!")
+    while gameOn == False:
+        startGame = str(raw_input("Ready? "))
+        if startGame == "start":
+            print("Let's play!")
+            gameOn = True
+            game()
+
+intro()
