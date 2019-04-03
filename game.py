8f1d11d3cc8c7c84d8b0fcd4adab0cde8d432ee2
fran496 <franciscorivas496@gmail.com>
2019-04-03 19:07:29 -0400

    Fixed some functions

--- game.py
+++ game.py
@@ -1,8 +1,26 @@
 from random import *
 
-gameOn = False
 randomNumber = 0
+gameOn = 0
 
+def intro ():
+    gameOn = False
+    playerName = str(raw_input("Hello! What's your name? "))
+    print("Welcome " + playerName + "!, write start when you're ready to play!")
+    while gameOn == False:
+        startGame = str(raw_input("Ready? "))
+        if startGame == "start":
+            print("Let's play!")
+            gameOn = True
+            game(gameOn)
+
+def game(status):
+    print("I'm going to choose an integer between 1 and 100, try to guess it!")
+    randomNumber = randint(1, 100) 
+    while status == True:
+        print("Player inputt")
+        playerInput = input()
+        testPlayerInput(playerInput)
 
 def testPlayerInput(playerInput):
     if playerInput == randomNumber:
@@ -16,22 +34,5 @@ def testPlayerInput(playerInput):
         print("THAT'S NOT IT! Guess lower!")
         return
 
-def game ():
-    print("I'm going to choose a natural number between 1 and 100, if you guess it right you win! ")
-    randomNumber = randint(1, 100)
-    
-    while gameOn == True:
-        playerInput = input()
-        testPlayerInput(playerInput)
-
-def intro ():
-    playerName = str(raw_input("Hello! What's your name? "))
-    print("Welcome " + playerName + "!, write start when you're ready to play!")
-    while gameOn == False:
-        startGame = str(raw_input("Ready? "))
-        if startGame == "start":
-            print("Let's play!")
-            gameOn = True
-            game()
-
 intro()
+game(gameOn)
