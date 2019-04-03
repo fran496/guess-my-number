5a62a94a08c7a5ccfc0b5aa2caaf7563bb35a26c
fran496 <franciscorivas496@gmail.com>
2019-04-03 17:14:28 -0400

    Created an introductory sequence for the game

--- game.py
+++ game.py
@@ -1,1 +1,11 @@
+gameOn = False
+userName = str(raw_input("Hello! Whats your name? "))
+print("Welcome " + userName + "!, write start when you're ready to play!")
 
+while gameOn == False:
+    startGame = str(raw_input("Ready? "))
+    if startGame == "start":
+        print("Let's play!")
+        gameOn = True
+
+# game()
