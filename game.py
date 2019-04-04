3f9c7a3161944407207638a81816e420966a7902
fran496 <franciscorivas496@gmail.com>
2019-04-03 20:42:23 -0400

    Stopped an error within the loop of the game function

--- game.py
+++ game.py
@@ -2,31 +2,32 @@ from random import *
 
 playerName = 0
 randomNumber = 0
-gameOn = 0
+status = 0
 
 def intro ():
-    gameOn = False
+    status = False
     playerName = str(raw_input("Hello! What's your name? "))
     print("Welcome " + playerName + "!, write start when you're ready to play!")
-    while gameOn == False:
-        startGame = str(raw_input("Ready? "))
-        if startGame == "start":
-            print("Let's play!")
-            gameOn = True
-            game(gameOn, playerName)
+    while status == False:
+		    startGame = str(raw_input("Ready? "))
+		    if startGame == "start":
+		        print("Let's play!")
+		        status = True
+		        game(status, playerName)
 
 def game(status, playerName):
     print("I'm going to choose an integer between 1 and 100, try to guess it!")
     randomNumber = randint(1, 100) 
     while status == True:
+        print("NEW GAME")
         playerInput = input()
         testPlayerInput(playerInput, playerName, randomNumber)
+
 
 def testPlayerInput(playerInput, playerName, randomNumber):
     if playerInput == randomNumber:
         print("YOU WIN " + playerName + "!")
-        status = False
-        return status
+        return
     elif playerInput < randomNumber:
         print("THAT'S NOT IT! Guess higher!")
         return
@@ -35,4 +36,3 @@ def testPlayerInput(playerInput, playerName, randomNumber):
         return
 
 intro()
-game(gameOn)
